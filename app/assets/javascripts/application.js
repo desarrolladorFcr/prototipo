1ec0a00c3e9009b64b6107f8517c90ef10e0fa0a
DESARROLLADOR_FCR <cifuentesrfelipe@gmail.com>
2018-09-03 21:05:54 -0500

    Se agrega datatable cdn

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,6 @@
 //= require jquery/dist/jquery.min.js
 //= require popper.js/dist/umd/popper.js
 //= require bootstrap/dist/js/bootstrap.min.js
-//= require datatables.net/js/jquery.dataTables.js
 //= require dom-factory/dist/dom-factory.js
 //= require material-design-kit/dist/material-design-kit.js
 
