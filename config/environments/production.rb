118a9d0320d393549385e491c7bb2e14c139c7ad
DESARROLLADOR_FCR <cifuentesrfelipe@gmail.com>
2018-09-03 22:13:31 -0500

    Se cambia el dominio del los documentos de ambiente

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.action_mailer.default_url_options = {host: 'http://localhost', port: 3000}
+  config.action_mailer.default_url_options = {host: 'https://publed.herokuapp.com/', port: 3000}
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
